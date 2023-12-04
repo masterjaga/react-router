e3b9d5340f33aeb999a4d32c7c4da8fab9e86798
masterjaga <145780229+masterjaga@users.noreply.github.com>
2023-12-04 15:15:35 +0530

    Update data.jsx

--- src/data.jsx
+++ src/data.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 
 const data1=[{
-     img :"https://www.guvi.in/blog/wp-content/uploads/2023/11/Feature-image-Top-High-Paying-Non-Coding-Jobs-in-Data-Science.webp",
-      title:"Top 10 High Paying Non-Coding Jobs in Data Science in 2023",
-      date:"November 28, 2023"
-    },
-    {
-      img :"https://www.guvi.in/blog/wp-content/uploads/2023/11/Feature-image-1-2048x1024.webp",
-      title:"Impact of Certification Programs on Hiring Data Scientists",
+     img :"https://www.guvi.in/blog/wp-content/uploads/2023/11/top-ways-to-assess-soft-skills-in-full-stack-developers.webp",
+      title:"Top Ways to Assess Soft Skills in Full Stack Developers in 2023",
       date:"November 15, 2023"
     },
     {
-      img :"https://www.guvi.in/blog/wp-content/uploads/2023/11/feature-image-product-based-companies-for-data-science-freshers.webp",
-      title:"Top Product-Based Companies for Data Science Freshers",
+      img :"https://www.guvi.in/blog/wp-content/uploads/2023/11/Feature-image-Full-Stack-Developer-vs-Software-Engineer-.webp",
+      title:"Top Differences: Full Stack Developer vs Software Engineer 2023",
+      date:"November 15, 2023"
+    },
+    {
+      img :"https://www.guvi.in/blog/wp-content/uploads/2023/11/Feature-image-Horizontal-vs-Vertical-Scaling-for-Efficient-System-Design.webp",
+      title:"Horizontal vs Vertical Scaling for Efficient System Design",
       date:"November 10, 2023"
 },
 
