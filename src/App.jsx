0d89db93da8d5aca2f5e3068a6c7494b95f28bc9
masterjaga <145780229+masterjaga@users.noreply.github.com>
2023-12-04 13:00:12 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,8 +19,8 @@ function App() {
       date:"4 December 2023"
     },
     {
-      img :"https://www.guvi.in/blog/wp-content/uploads/2023/12/Best-AI-Tools-for-Coding-01-2048x1072.png",
-      title:"6 Best AI Tools for Coding",
+      img :"https://www.guvi.in/blog/wp-content/uploads/2023/11/ai_tools_for_content_creation.webp",
+      title:"Top 9 AI Tools for Content Creation That You Shouldn’t Miss",
       date:"2 December 2023"
     },
     {
@@ -29,44 +29,44 @@ function App() {
       date:"2 December 2023"
     },
     {
-      img :"https://www.applify.com.sg/blog/wp-content/uploads/2023/06/What-is-full-stack-developer-768x432.webp",
-      title:"Top 8 Blockchain Project Ideas For Students",
-      date:"D 2, 2023"
+      img :"https://www.guvi.in/blog/wp-content/uploads/2023/11/top-ways-to-assess-soft-skills-in-full-stack-developers.webp",
+      title:"Top Ways to Assess Soft Skills in Full Stack Developers in 2023",
+      date:"November 15, 2023"
     },
     {
-      img :"https://www.guvi.in/blog/wp-content/uploads/2023/08/Feature-image-Best-Full-Stack-Development-Online-Courses.webp",
-      title:"Top Skills To Become a Full-Stack Developer in 2023",
-      date:"August 3, 2023"
+      img :"https://www.guvi.in/blog/wp-content/uploads/2023/11/Feature-image-Full-Stack-Developer-vs-Software-Engineer-.webp",
+      title:"Top Differences: Full Stack Developer vs Software Engineer 2023",
+      date:"November 15, 2023"
     },
     {
-      img :"https://www.guvi.in/blog/wp-content/uploads/2023/07/roles_and_responsibilities_of_full_stack_developer_1x.webp",
-      title:"The Future & Scope of Full-Stack Developers in India",
-      date:"July 27, 2023"
+      img :"https://www.guvi.in/blog/wp-content/uploads/2023/11/Feature-image-Horizontal-vs-Vertical-Scaling-for-Efficient-System-Design.webp",
+      title:"Horizontal vs Vertical Scaling for Efficient System Design",
+      date:"November 10, 2023"
     },
     {
-      img :"https://www.guvi.in/blog/wp-content/uploads/2023/07/best-data-science-frameworks.webp",
-      title:"Top 10 Data Science Tools in 2023",
-      date:"November 1, 2023"
+      img :"https://www.guvi.in/blog/wp-content/uploads/2023/11/Feature-image-Top-High-Paying-Non-Coding-Jobs-in-Data-Science.webp",
+      title:"Top 10 High Paying Non-Coding Jobs in Data Science in 2023",
+      date:"November 28, 2023"
     },
     {
-      img :"https://www.guvi.in/blog/wp-content/uploads/2023/09/Feature-image-Useful-Python-Libraries-and-Tools-For-Data-Science-Beginners.webp",
-      title:"Useful Python Libraries & Tools for Data Science Beginners",
-      date:"September 29, 2023"
+      img :"https://www.guvi.in/blog/wp-content/uploads/2023/11/Feature-image-1-2048x1024.webp",
+      title:"Impact of Certification Programs on Hiring Data Scientists",
+      date:"November 15, 2023"
     },
     {
-      img :"https://www.tqmi.com/wp-content/uploads/2022/02/Data-Science-4.webp",
-      title:"Best Way to Learn Data Science in 2023",
-      date:"July 28, 2023"
+      img :"https://www.guvi.in/blog/wp-content/uploads/2023/11/feature-image-product-based-companies-for-data-science-freshers.webp",
+      title:"Top Product-Based Companies for Data Science Freshers",
+      date:"November 10, 2023"
     },
     {
-      img :"https://www.proschoolonline.com/wp-content/uploads/2021/02/Cyber-Security-768x461.webp",
-      title:"Top 10 Ethical Hacking Books for Beginner to Advanced",
-      date:"November 21, 2022"
+      img :"https://www.guvi.in/blog/wp-content/uploads/2023/09/Feature-image-What-Is-Hacking_-Types-of-Hacking-More.webp",
+      title:"What Is Hacking? Types of Hacking & More",
+      date:"September 25, 2023"
     },
     {
-      img :"https://easydmarc.com/blog/wp-content/webp-express/webp-images/doc-root/wp-content/uploads/2022/05/What-is-Password-Attack-in-Cyber-Security_.jpg.webp",
-      title:"8 Different Types of Cybersecurity and Threats Involved",
-      date:"November 9, 2022"
+      img :"https://www.guvi.in/blog/wp-content/uploads/2021/03/The-Ultimate-%E2%80%A8Cybersecurity-Roadmap-for-Beginners.webp",
+      title:"The Ultimate Cybersecurity Roadmap for Beginners",
+      date:"March 1, 2021"
     },
     {
       img :"https://www.wrk.com/media/images/sales-cyber-security.2e16d0ba.fill-1600x900.format-webp.webp",
@@ -74,19 +74,19 @@ function App() {
       date:"November 10, 2020"
     },
     {
-      img :"https://www.guvi.in/blog/wp-content/uploads/2023/10/Feature-image-Product-based-Company-Vs-Service-based-Company.webp",
-      title:"Product-based Company Vs Service-based Company: Which is Best Choice?",
-      date:"November 2, 2023"
+      img :"https://www.guvi.in/blog/wp-content/uploads/2023/12/Feature-image-Top-IT-Jobs-for-Commerce-Students.webp",
+      title:"Top IT Jobs for Commerce Students: A Lucrative Career Path",
+      date:"December 2, 2023"
     },
     {
-      img :"https://www.guvi.in/blog/wp-content/uploads/2023/10/Feature-image-Top-Product-Based-Companies-for-Machine-Learning-Engineers.webp",
-      title:"Top 5 Product-Based Companies for Machine Learning Engineers",
-      date:"October 11, 2023"
+      img :"https://www.guvi.in/blog/wp-content/uploads/2023/12/Feature-image-Automation-Test-Engineer-Resume-Important-Things-To-Consider.webp",
+      title:"Automation Test Engineer Resume: 10 Important Things To Consider",
+      date:"December 1, 2023"
     },
     {
-      img :"https://www.guvi.in/blog/wp-content/uploads/2023/09/Feature-image-Everything-You-Should-Know-About-ChatGPT-and-Why-It-Matters.webp",
-      title:"Everything You Should Know About ChatGPT & Why It Matters?",
-      date:"September 29, 2023"
+      img :"https://www.guvi.in/blog/wp-content/uploads/2023/11/Featured-Image-2048x1072.webp",
+      title:"9 Best Product-Based Companies for Project Management",
+      date:"December 1, 2023"
     }]
    
     console.log(datas)
